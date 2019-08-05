aa47c13a7a7b61ee8fd8565ca835cbb9ab014f63
pswanson2 <43864136+pswanson2@users.noreply.github.com>
2019-08-05 16:47:02 -0500

    More changes to Main.java

--- application/Main.java
+++ application/Main.java
@@ -18,6 +18,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
@@ -68,68 +69,97 @@ public class Main extends Application {
                 public void handle(ActionEvent event) {
                     
                     /* Build the query */
-                    String q = new String("SELECT r.title, a.artist_name, la.label_name, li.format, li.price FROM "+ 
-                    "Album r, Artist a, Label la, Listing li WHERE");
+                    String q = new String("SELECT r.title, a.artist_name, la.label_name, li.format, li.price FROM " + 
+                            "Album r, Artist a, Label la, Listing li WHERE r.release_id = li.release_id AND r.label_id = la.label_id " + 
+                            "AND r.artist_id = a.artist_id");
                     boolean first_flag = true; //check if this is the first condition. Used to properly format the query
                     if(!titleSearch.getText().isEmpty() && titleSearch.getText() != null) {
-                        if(first_flag) {
-                            
-                            first_flag = false;
-                        } else {
-                            
-                        }
+                        q += " AND r.title LIKE \"%" + titleSearch.getText() + "%\"";
+                        first_flag = false;
                     }
                     if(!artistSearch.getText().isEmpty() && artistSearch.getText() != null) {
-                        if(first_flag) {
-                            
-                            first_flag = false;
-                        } else {
-                            
-                        }
+                        q += " AND a.artist_name LIKE \"%" + artistSearch.getText() + "%\"";
+                        first_flag = false;
                     }
                     if(!labelSearch.getText().isEmpty() && labelSearch.getText() != null) {
-                        if(first_flag) {
-                            
-                            first_flag = false;
-                        } else {
-                            
-                        }
+                        q += " AND la.label_name LIKE \"%" + labelSearch.getText() + "%\"";
+                        first_flag = false;
                     }
                     if(!formatSearch.getText().isEmpty() && formatSearch.getText() != null) {
-                        if(first_flag) {
-                            
-                            first_flag = false;
-                        } else {
-                            
-                        }
+                        q += " AND li.format LIKE \"%" + formatSearch.getText() + "%\"";
+                        first_flag = false;
                     }
                     if(!priceSearch.getText().isEmpty() && priceSearch.getText() != null) {
-                        if(first_flag) {
-                            
-                            first_flag = false;
-                        } else {
-                            
-                        }
+                        q += " AND li.price <= " + priceSearch.getText();
+                        first_flag = false;
+                    }
+                    q += ";";
+                    System.out.println(q); //TODO remove
+                    
+                    /* Only submit a querry if they typed something into one of the fields */
+                    if(!first_flag) {
+                        myDB.Query(q);
                     }
                     
-                    
+                    /* Output the result */
                     final Stage dialog = new Stage();
                     dialog.initModality(Modality.APPLICATION_MODAL);
                     dialog.initOwner(primaryStage);
                     VBox dialogVbox = new VBox();
-                    
-                    
-                    
                     Scene dialogScene = new Scene(dialogVbox, 300, 200);
                     dialog.setScene(dialogScene);
-
                     dialog.show();
                 }
             });
             
+            Button randomListing = new Button("Random Listing");
+            randomListing.setTooltip(new Tooltip("Click to find a random listing"));
+            Search.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    
+                }
+            });
+            
+            
+            /* Begin building center */
+            TableView<String[]> searchResults = new TableView<String[]>();
+            
+            
+            
+            
+            /* Begin building right */
+            TableView<String[]> cart = new TableView<String[]>();
+            
+            Button refreshCart = new Button("Refresh Cart");
+            refreshCart.setTooltip(new Tooltip("Click to refresh the cart"));
+            refreshCart.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    String cartQuery = new String("SELECT r.title, a.artist_name, li.format, li.price from ");
+                }
+            });
+            
+            Button delete = new Button("Delete");
+            delete.setTooltip(new Tooltip("Delete selected items from cart"));
+            delete.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                    
+                }
+            });
+            
             /* Populate VBoxes */
-            left.getChildren().addAll(new Label("Title"), titleSearch, new Label("Artist"), artistSearch, new Label("Label"), labelSearch, 
-                    new Label("Format"), formatSearch,new Label("Max Price"), priceSearch, Search);
+            left.getChildren().addAll(new Label("\n"), new Label("Submit a Query"), new Label("\n"), new Label("Title"), titleSearch, new Label("\n"), 
+                    new Label("Artist"), artistSearch, new Label("\n"), new Label("Label"), labelSearch, 
+                    new Label("\n"), new Label("Format"), formatSearch, new Label("\n"), new Label("Max Price"), priceSearch, 
+                    new Label("\n"), Search, new Label("\n"), randomListing);
+            left.setPrefWidth(200);
+            
+            center.getChildren().addAll(new Label("\n"), new Label("Search Results"), new Label("\n"), searchResults);
+            
+            right.getChildren().addAll(new Label("\n"), new Label("Cart"), new Label("\n"), cart, new Label("\n"), refreshCart, new Label("\n"), delete);
+            right.setPrefWidth(300);
             
             /* Place the VBoxes */
             root.setLeft(left);
